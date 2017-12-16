ef9e7f637e8ce02ffdf0cd570941715466f6d9eb
Nicolaj Pedersen <nicolajstaldpedersen@gmail.com>
2017-12-16 15:10:14 +0100

    Initial commit

---
+++ app/src/main/java/appstuio60/billardboard/Game/GameActivity.java
@@ -0,0 +1,10 @@
+package appstuio60.billardboard.Game;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Nicol on 19-07-2017.
+ */
+
+public class GameActivity extends AppCompatActivity {
+}
---
+++ app/src/main/java/appstuio60/billardboard/LobbyFragment.java
@@ -0,0 +1,10 @@
+package appstuio60.billardboard;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by Nicol on 19-07-2017.
+ */
+
+public class LobbyFragment extends Fragment {
+}
